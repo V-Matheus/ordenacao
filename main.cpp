361b2821835d5f049576c8c8f860d507c9f12c69
Victor Matheus <victormatheus507@gmail.com>
2025-02-16 17:08:42 -0300

    lendo arquivos

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "ordenacao.cpp"
 #include <iostream>
 #include <chrono>
+#include <fstream>
+#include <cstdlib>
+
+#ifdef _WIN32
+  #include <direct.h>
+  #define getcwd _getcwd
+#else
+  #include <unistd.h>
+#endif
 
 void testSortFunction(void (*sortFunc)(int[], unsigned int), int a[], unsigned int t, const std::string& sortName) {
   int* copy = new int[t];
@@ -14,27 +23,48 @@ void testSortFunction(void (*sortFunc)(int[], unsigned int), int a[], unsigned i
 
   std::chrono::duration<double> duration = end - start;
 
-  sortFunc(copy, t);
-
-  if(ordenado(copy, t)) {
-    std::cout << "Array ordenado \n\n";
+  if (ordenado(copy, t)) {
+    std::cout << "Array ordenado ";
     std::cout << "Tempo de execucao: " << duration.count() << " segundos" << std::endl;
   } else {
-    std::cout << "Array não ordenado ";
+    std::cout << "Array nao ordenado ";
   }
 
   std::cout << "- - - - - - - - - - - - - - - - - - \n\n";
-
 
   delete[] copy;
 }
 
 int main() {
-  int a[10] = {2, 8, 5, 7, 3, 6, 4, 9, 1, 12};
+  char cwd[1024];
+  if (getcwd(cwd, sizeof(cwd)) == nullptr) {
+    std::cerr << "Erro ao obter diretório atual." << std::endl;
+    return 1;
+  }
 
-  testSortFunction(selecao, a, 10, "Selecao");
-  testSortFunction(insercao, a, 10, "Insercao");
-  testSortFunction([](int arr[], unsigned int t) { merge_sort(arr, 0, t - 1); }, a, 10, "Merge Sort");
+  std::string filePath = std::string(cwd) + "\\testes\\caso01\\exemplo-10000-1.txt";
 
+  std::ifstream file(filePath);
+  if (!file) {
+    std::cerr << "Erro ao abrir o arquivo de teste: " << filePath << std::endl;
+    return 1;
+  }
+
+  unsigned int t;
+  file >> t;
+  std::cout << "Tamanho do array: " << t << std::endl;
+
+  int* a = new int[t];
+  for (unsigned int i = 0; i < t; ++i) {
+    file >> a[i];
+  }
+
+  file.close();
+
+  testSortFunction(selecao, a, t, "Selecao");
+  testSortFunction(insercao, a, t, "Insercao");
+  testSortFunction([](int arr[], unsigned int t) { merge_sort(arr, 0, t - 1); }, a, t, "Merge Sort");
+
+  delete[] a;
   return 0;
 }
